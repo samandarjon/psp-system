cd7dce078bfe666d3731b787149d954aefff9665
samandar_akbarov <akbarov.samandar@gmail.com>
2025-10-21 14:06:12 +0500

    Bing bang

---
+++ src/main/java/live/akbarov/pspsystem/PspSystemApplication.java
@@ -0,0 +1,13 @@
+package live.akbarov.pspsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PspSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PspSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/live/akbarov/pspsystem/PspSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package live.akbarov.pspsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PspSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
